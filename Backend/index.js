05e715477daa728d62b309fb5b211da67c952fca
sanjeetk5 <112753484+sanjeetk5@users.noreply.github.com>
2023-07-13 15:15:58 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -3,8 +3,10 @@ const { connection } = require("./config/db");
 const { userRouter } = require("./routes/User.routes");
 const { auth } = require("./middleware/auth.middleware");
 const { employeeRouter} = require("./routes/Employee.routes");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
+app.use(cors());
 app.use(express.json());
 
 app.use("/user", userRouter);
