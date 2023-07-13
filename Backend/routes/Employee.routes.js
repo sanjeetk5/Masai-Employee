068babfea5aca7c2ff7407cb025aebcf010581de
sanjeetk5 <112753484+sanjeetk5@users.noreply.github.com>
2023-07-13 14:28:42 +0530

    done

--- Backend/routes/Employee.routes.js
+++ Backend/routes/Employee.routes.js
@@ -27,6 +27,22 @@ employeeRouter.get("/" , async(req,res)=>{
     }
 })
 
+employeeRouter.delete("/delete/:empID" , async(req,res)=>{
+    const {empID} = req.params;
+    const employee = await EmployeeModel.findOne({_id : postID })
+    try {
+        if(req.body.empID == employee.empID){
+            await EmployeeModel.findByIdAndDelete({_id : empID} , req.body)
+            res.status(200).send({"msg" : `The note with id: ${empID} is deleted`})
+        }else{
+            res.status(400).send({"err" : "you are not authorized to delete"})
+        }
+    } catch (err) {
+        res.status(400).send({err:err.message})
+    }
+
+})
+
 module.exports = {
     employeeRouter,
   };
